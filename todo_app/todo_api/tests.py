22ec8bbf8cd6137ff3156fd6be34ea8abba7c22c
Rizki Perdana <perdana.rizki16@gmail.com>
2024-05-08 20:55:33 +0700

    feat: additional coverage unit and report

--- todo_app/todo_api/tests.py
+++ todo_app/todo_api/tests.py
@@ -19,7 +19,7 @@ from todo_api.views import TodoViewSet
 User = get_user_model()
 faker = Faker()
 
-def init_baker(user, text, is_completed):
+def init_baker(user, text=faker.text(), is_completed=True):
     return baker.make(
         Todo, 
         user=user,
@@ -35,10 +35,12 @@ class TestApi(APITestCase):
         self.client.force_authenticate(self.user)
         self.todo = baker.make("Todo", user=self.user)
     
+    
     def test_list(self):
         url = '/todo_api/'
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
     
     def test_create(self):
         url = '/todo_api/'
@@ -49,6 +51,7 @@ class TestApi(APITestCase):
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         
+        
     def test_update(self):
         url = f'/todo_api/{self.todo.id}/'
         data = {
@@ -58,10 +61,112 @@ class TestApi(APITestCase):
         response = self.client.put(url, data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         
+        
     def test_delete(self):
         url = f'/todo_api/{self.todo.id}/'
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+
+class TestView(APITestCase):
+    def setUp(self):
+        self.view = TodoViewSet
+        self.factory = APIRequestFactory()
+        self.user = baker.make(User)
+        self.todo1 = init_baker(self.user, is_completed=True)
+        self.todo2 = init_baker(self.user, is_completed=False)
+        self.todo3 = init_baker(self.user, is_completed=True)
+    
+    
+    def test_serializer(self):
+        view = self.view()
+        view.action = 'list'
+        self.assertEqual(view.get_serializer_class(), TodoSerializer)
+    
+    
+    def test_deserializer(self):
+        view = self.view()
+        view.action = 'post'
+        self.assertEqual(view.get_serializer_class(), TodoDeSerializer)
+    
+    
+    def test_queryset(self):
+        view = self.view()
+        request = self.factory.get('/')
+        request.user = self.user
+        view.request = request
+        expected = [self.todo1.id, self.todo2.id, self.todo3.id]
+        actual = view.get_queryset()
+        
+        self.assertQuerysetEqual(actual, expected, lambda item: item.id, ordered=False)
+        
+    
+    def test_list(self):
+        view = self.view.as_view({'get': 'list'})
+        request = self.factory.get(reverse('todo_api-list'))
+        force_authenticate(request, self.user)
+        view.request = request
+        response = view(request)
+        
+        self.assertEqual(len(response.data), 3)
+        self.assertEqual(
+            [self.todo1.id, self.todo2.id, self.todo3.id],
+            [todo['id'] for todo in response.data]
+        )
+        
+    
+    def test_get(self):
+        view = self.view.as_view({'get': 'retrieve'})
+        request = self.factory.get(reverse('todo_api-detail', kwargs={'pk': self.todo1.pk}))
+        force_authenticate(request, self.user)
+        response = view(request, pk=self.todo1.pk)
+        self.assertEqual(response.status_code, 200) 
+        self.assertEqual(response.data['id'], self.todo1.pk)
+
+
+    def test_create(self):
+        view = self.view.as_view({'post': 'create'})
+        data = {
+            "title": faker.text(),
+            "description": faker.paragraph(),
+            "is_completed": faker.boolean(),
+        }
+        
+        request = self.factory.post(reverse('todo_api-list'), data=data)
+        force_authenticate(request, self.user)
+        view.request = request
+        response = view(request)
+        
+        self.assertEqual(response.data['title'], data['title'])
+        
+    
+    def test_update(self):
+        _title = faker.text()
+        _description = faker.paragraph()
+        _status = faker.boolean()
+        data = {
+            "title": _title,
+            "description": _description,
+            "is_completed": _status,
+        }
+        view = self.view.as_view({'put': 'update'})
+        request = self.factory.put(reverse('todo_api-detail', kwargs={'pk': self.todo1.pk}), data=data)
+        force_authenticate(request, self.user)
+        response = view(request, pk=self.todo1.pk)
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['title'], _title)
+        self.assertEqual(response.data['description'], _description)
+        self.assertEqual(response.data['is_completed'], _status)
+        
+
+        def test_delete(self):
+            view = self.view.as_view({'delete': 'destroy'})
+            request = self.factory.delete(reverse('todo_api-detail', kwargs={'pk': self.todo1.pk}))
+            force_authenticate(request, self.user)
+            response = view(request, pk=self.todo1.pk)
+            
+            self.assertEqual(response.status_code, 204) 
 
 
 class TestSerializer(APITestCase):
@@ -70,6 +175,7 @@ class TestSerializer(APITestCase):
         self.factory = APIRequestFactory()
         self.user = baker.make(User)
         self.todo1 = init_baker(self.user, 'test 1', True)
+        
         
     def test_serialize(self):
         expected_data = {
@@ -92,6 +198,7 @@ class TestDeserializer(APITestCase):
         self.user = baker.make(User)
         self.todo1 = init_baker(self.user, 'test 1', False)
         self.maxDiff = None
+    
     
     def test_deserialize(self):
         title = faker.text()
